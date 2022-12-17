b945b49e6a9cb146574c315e18875dad35838b55
haZuny <gkwns5791@naver.com>
2022-12-17 18:12:27 +0900

    배경 수정

--- lib/testPage.dart
+++ lib/testPage.dart
@@ -16,12 +16,26 @@ class TestPage extends StatelessWidget{
     child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+          // mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            // 앱 상단 로고
+            Container(
+              padding: EdgeInsets.only(bottom: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 7 / 100),
+              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 25 / 100,
+              width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 10 / 100,
+              // color: Colors.red,
+              child: Image.asset('assets/img/icon.png',)
+              // child: Column(
+              //   children: [
+              //
+              //   ],
+              // ),
+            ),
             // 세로 화면의 25% 비율부터 시작
             Container(
-              height: MediaQuery.of(context).size.height * 75 / 100,
+              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 75 / 100,
               width: MediaQuery.of(context).size.width,
+              // 여기서부터 찐 개발 시작
               child: Column(
                 children: [
                   ElevatedButton(onPressed: ()=>null, child: Text("여기서부터 개발하시면 됩니다"))
