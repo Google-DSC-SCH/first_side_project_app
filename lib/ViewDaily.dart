0336869f873221cab3e9d66cf25448232cf300a3
haZuny <gkwns5791@naver.com>
2022-12-17 20:57:21 +0900

    기본 폰트 변경

--- lib/ViewDaily.dart
+++ lib/ViewDaily.dart
@@ -39,33 +39,116 @@ class _ViewDaily extends State<ViewDaily> {
                     child: Image.asset(
                       'assets/img/icon.png',
                     )),
+
+                // Body
                 // 세로 화면의 22% 비율부터 시작
                 Container(
-                  height: (MediaQuery.of(context).size.height -
-                          MediaQuery.of(context).padding.top) *
-                      78 /
-                      100,
-                  width: MediaQuery.of(context).size.width,
-                  // 여기서부터 찐 개발 시작
-                  child: Column(
-                    children: [
-                      Text("제목"),
-                      Card(
-                          shape: RoundedRectangleBorder(  //모서리를 둥글게 하기 위해 사용
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          color: Color(0xFFFAF4B7),
-                          elevation: 0, // 그림자 깊이
-                          child: Container(
-                            width: getMobileSizeFromPercent(context, 80, true),
-                            height: 50,
-                            child: Text("aa")
+                    height: (MediaQuery.of(context).size.height -
+                            MediaQuery.of(context).padding.top) *
+                        78 /
+                        100,
+                    width: MediaQuery.of(context).size.width,
+                    // 여기서부터 찐 개발 시작
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          // 제목
+                          Column(
+                            children: [
+                              Text("제목", style: TextStyle(fontSize: 30)),
+                              Card(
+                                shape: RoundedRectangleBorder(
+                                  //모서리를 둥글게 하기 위해 사용
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                color: Color(0xFFFAF4B7),
+                                elevation: 0, // 그림자 깊이
+                                child: Container(
+                                    width: getMobileSizeFromPercent(
+                                        context, 80, true),
+                                    child: Text("aa")),
+                              ),
+                            ],
                           ),
 
-                      )
-                    ],
-                  ),
-                )
+                          // 설명
+                          Column(
+                            children: [
+                              Text("설명"),
+                              Card(
+                                shape: RoundedRectangleBorder(
+                                  //모서리를 둥글게 하기 위해 사용
+                                  borderRadius: BorderRadius.circular(16.0),
+                                ),
+                                color: Color(0xFFFAF4B7),
+                                elevation: 0, // 그림자 깊이
+                                child: Container(
+                                    width: getMobileSizeFromPercent(
+                                        context, 80, true),
+                                    child: Text("aa")),
+                              ),
+                            ],
+                          ),
+
+                          // 반복 요일
+                          Container(
+                            width: getMobileSizeFromPercent(context, 80, true),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [Text("반복요일"), Text("월수금")],
+                            ),
+                          ),
+
+                          // 알림
+                          Container(
+                            width: getMobileSizeFromPercent(context, 80, true),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [Text("알림", style: TextStyle(fontFamily: "HandTyping")), Text("ON"), Text("오후 7:30")],
+                            ),
+                          ),
+
+                          // 상태
+                          Container(
+                            width: getMobileSizeFromPercent(context, 80, true),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("상태"),
+                                ElevatedButton(
+                                  onPressed: () => null,
+                                  child: Text("완료"),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => null,
+                                  child: Text("미완료"),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          // 뒤로가기
+                          Container(
+                            width: getMobileSizeFromPercent(context, 80, true),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () => null,
+                                  child: Text("수정"),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => null,
+                                  child: Text("삭제"),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => null,
+                                  child: Text("뒤로"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ]))
               ],
             )),
       );
