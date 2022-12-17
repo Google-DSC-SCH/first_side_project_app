ea1c4d1ffe88bac77b428e25f8969e50fad2c3ca
haZuny <gkwns5791@naver.com>
2022-12-17 17:47:45 +0900

    상태바 높이 띄우기

--- lib/ViewDaily.dart
+++ lib/ViewDaily.dart
@@ -8,6 +8,8 @@ class ViewDaily extends StatefulWidget{
 class _ViewDaily extends State<ViewDaily>{
   @override
   Widget build(BuildContext context) => Container(
+    // 상태바 높이만큼 띄우기
+    margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
     // 배경 이미지 적용
     decoration: BoxDecoration(
         image: DecorationImage(
@@ -20,9 +22,9 @@ class _ViewDaily extends State<ViewDaily>{
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          // 세로 화면의 22% 비율부터 시작
+          // 세로 화면의 25% 비율부터 시작
           Container(
-            height: MediaQuery.of(context).size.height * 78 / 100,
+            height: MediaQuery.of(context).size.height * 75 / 100,
             width: MediaQuery.of(context).size.width,
             child: Column(
               children: [
