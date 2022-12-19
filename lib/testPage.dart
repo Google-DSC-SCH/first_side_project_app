d8fe440f870255195d21dcc265e2314004be63f9
hajunKwon <gkwns5791@naver.com>
2022-12-19 17:54:17 +0900

    배경 수정, testpage 수정

--- lib/testPage.dart
+++ lib/testPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'getMediaQuery.dart';
+
 class TestPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) => Container(
@@ -14,30 +16,33 @@ class TestPage extends StatelessWidget{
         )
     ),
     child: Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(getMobileSizeFromPercent(context, 18, false)),
+          child: Container(color: Colors.transparent,child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+            children: [
+              Container(child: Image.asset('assets/img/icon.png'), height: getMobileSizeFromPercent(context, 10, false),),
+              Container()
+            ],
+          ),),
+        ),
         body: Column(
-          // mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            // 앱 상단 로고
+            // Body
             Container(
-              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 22 / 100,
-              width: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 10 / 100,
-              padding: EdgeInsets.only(bottom: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 5 / 100),
-              child: Image.asset('assets/img/icon.png',)
-            ),
-            // 세로 화면의 22% 비율부터 시작
-            Container(
-              height: (MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top) * 78 / 100,
-              width: MediaQuery.of(context).size.width,
-              // 여기서부터 찐 개발 시작
-              child: Column(
-                children: [
-                  ElevatedButton(onPressed: ()=>null, child: Text("여기서부터 개발하시면 됩니다"))
-                ],
-              ),
-            )
-          ]
-          ,)
-    ),
+                height: getMobileSizeFromPercent(context, 82, false) - MediaQuery.of(context).padding.top * 2,
+                width: double.infinity,
+                // 여기서부터 찐 개발 시작
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                          Text("여기서부터 개발하시면 됩니다",
+                              style: TextStyle(fontSize: 20)),
+                    ]))
+          ],
+        )),
   );
 }
